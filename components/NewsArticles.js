8d5d29c13ea1f0d1f0444d4d5cda22583c47c194
unknown <pranavchandra27@gmail.com>
2020-12-08 12:52:18 +0530

    refractor NewsArticle component

--- components/NewsArticles.js
+++ components/NewsArticles.js
@@ -2,42 +2,31 @@ import { useDataLayer } from "../components/StateProvider";
 
 const NewsArticles = () => {
   const {
-    state: { articles, query },
+    state: { articles },
   } = useDataLayer();
 
   return (
     <div className="news-articles">
-      {!articles ? (
-        <h2>Loading...</h2>
-      ) : (
-        <>
-          <h2>{query ? `You searched for ${query}` : "Latest Articles"} </h2>
-          <div className="grid">
-            {articles?.map((article) => (
-              <div key={article.short_url} className="article">
-                <img
-                  src={article.multimedia[3].url}
-                  alt={article.multimedia[3].caption}
-                />
-                <div className="info">
-                  <h4>
-                    {(article.title
-                      ? article.title
-                      : article.abstract
-                    ).substring(0, 50)}
-                    ...
-                  </h4>
-                  <p>
-                    {article.published_date
-                      ? article.published_date
-                      : article.pub_date}
-                  </p>
-                  <button>View Article</button>
-                </div>
+      <h2>Latest Articles</h2>
+      {articles.length && (
+        <div className="grid">
+          {articles?.map((article) => (
+            <div key={article.short_url} className="article">
+              <img
+                src={article.multimedia[3].url}
+                alt={article.multimedia[3].caption}
+              />
+              <div className="info">
+                <h4>
+                  {article.title.substring(0, 50)}
+                  ...
+                </h4>
+                <p>{article.published_date}</p>
+                <button>View Article</button>
               </div>
-            ))}
-          </div>
-        </>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
